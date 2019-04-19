979f38a55390b5c3d515e1c505cf03b1ce42a8c4
Ahmed Adel <w33zy.7@gmail.com>
2019-04-19 15:14:26 +0200

    ♻️ import HTTPClient and Forms Modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
+
     BrowserModule,
     AppRoutingModule
   ],
