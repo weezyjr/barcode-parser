a4facb68caf3817da45e534deb1ed1ef51f24866
Ahmed Adel <w33zy.7@gmail.com>
2019-04-19 14:46:01 +0200

    ⭐ add parse barcode endpoint
    GET api/v1/barcode/parse/:barcode

--- api/src/controller/parseBarcode.ctrl.js
+++ api/src/controller/parseBarcode.ctrl.js
@@ -1,10 +1,36 @@
-// create new barcode format
+const Formats = require('../model').Formats;
+const parseFunction = require('../parseBarcodeService');
+
 module.exports.parse = async function (req, res, next) {
     try {
         // check if the request is valid
         if (!req || !req.params || !req.params.barcode)
             throw new Error('Bad request');
 
+        const barcode = req.params.barcode;
+
+        await Formats.findAll()
+            .then((formats) => {
+
+                let results = [];
+
+                for(let format of formats){
+                    const parsedBarcode = parseFunction(barcode, format);
+                    if(parsedBarcode){
+                        results.push({
+                            format,
+                            parsedBarcode
+                        });
+                    }
+                }
+                
+                // response
+                res.status(200).json({
+                    status: 'success',
+                    message: 'barcode parsed successfly',
+                    data: results
+                })
+            });
 
     } catch (error) {
         next(error);
